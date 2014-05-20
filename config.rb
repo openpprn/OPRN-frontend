3d41477fcfce926970ce588eaa5b715c0100bf75
seanahrens <seanahrens@gmail.com>
2014-05-20 09:05:16 -0700

    no message

--- config.rb
+++ config.rb
@@ -38,7 +38,7 @@ page "*", :layout => "layout.haml"
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
